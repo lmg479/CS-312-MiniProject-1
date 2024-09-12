ed852efaff296931f8c7f77442d24bef90c775ff
Laura Guerrero <116206936+lmg479@users.noreply.github.com>
2024-09-11 21:17:27 -0700

    Update express.js

--- express.js
+++ express.js
@@ -1,58 +1,73 @@
+// imports
+import express from "express";
+import bodyParser from "body-parser";
+
 // constants
-var express = require('express');
-var app = express();
-const port = 8080;
-const blogPosts = [];
+const app = express();
+const port = 8080
+const current_date = new Date();
+
+// create the empty array
+var blogPosts = [];
 
 // setting up the ejs and url
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // deal with blog posts
 app.get('/', (req, res) => 
 {
-    // get the index and array of each post
-    res.render('index', 
-    { 
-        blogPosts: blogPosts 
-    });
+    // access html script
+    res.render('index.ejs', { blogPosts });
 });
 
 // add a new post to the array
-app.post('/new', (req, res) => 
+app.post('/create-form', (req, res) => 
 {
-    // get data and put it into a new post array
-    const { title, content, otherInfo, name } = req.body;
-    const newPost = { title, content, otherInfo, 
-                        name, creationTime: new Date()};
+    // get data of each of the contents of new post
+    var title = req.body.title, content = req.body.content;
+    var otherInfo = req.body.otherInfo, name = req.body.name;
 
-    // push and redirect
-    blogPosts.push(newPost);
+    // put each of the variables into the array
+    var newPost = { title, content, otherInfo, 
+                        name, creationTime: current_date};
+
+    // add the new post to the next array
+    blogPosts[blogPosts.length] = newPost;
+
+    // redirect back to homepage
     res.redirect('/');
 });
 
 // render the edit pages for posts
 app.get('/edit/:id', (req, res) => 
 {
-    // get the exact position in the array
-    const postId = req.params.id;
-    const post = blogPosts[postId];
-    res.render('edit', { post: post, postId: postId });
+    // get the new elemets of the array
+    var postId = req.params.id;
+
+    // get the index of each blog post 
+    var post = blogPosts[postId];
+
+    // get the information from the edit section of the html
+    res.render('edit', { post, postId });
 });
 
 // update posts
 app.post('/edit/:id', (req, res) => 
 {
     // get the array
-    const postId = req.params.id;
+    var postId = req.params.id;
 
     // update the array with the new edits if any
     blogPosts[postId] = 
     { 
-        title: req.body.title, content: req.body.content,
-            otherInfo: req.body.otherInfo, name: req.body.name,
-                        creationTime: blogPosts[postId].creationTime 
+        // update title or keep the old title
+        title: req.body.title || blogPosts[postId].title,
+        content: req.body.content || blogPosts[postId].content,
+        otherInfo: req.body.otherInfo || blogPosts[postId].otherInfo,
+        name: req.body.name || blogPosts[postId].name,
+        creationTime: blogPosts[postId].creationTime 
     };
     // redirect back to homepage
     res.redirect('/');
@@ -66,5 +81,10 @@ app.post('/delete/:id', (req, res) => {
     res.redirect('/');
 
 });
+
 // start server at the port
-app.listen(port, () => {});
+app.listen(port, () => 
+    {
+        console.log(`Server is running on port ${port}.`);
+    });
+
